83219a6bd1ee1a0304ee25979b6faa33649aee14
renaldork <97715678+renaldork@users.noreply.github.com>
2022-04-17 17:44:06 +0700

    Add files via upload

---
+++ ProblemSet2_1/ProblemSet2_1/problemset2_1/ProblemSet2_1.java
@@ -0,0 +1,18 @@
+package problemset2_1;
+
+public class ProblemSet2_1 {
+    public static void horn (){
+    System.out.println(" ___         ___");
+    System.out.println("|   |       |   |");
+    System.out.println("|   |       |   |");
+    }
+public static void main(String[] args) {
+        horn();
+        System.out.println("-----------------");
+        System.out.println("-    ^     ^    -");
+        System.out.println("-       |       -");
+        System.out.println("-   \\______/    -");
+        System.out.println("-               -");
+        System.out.println("-----------------");
+    }   
+}
